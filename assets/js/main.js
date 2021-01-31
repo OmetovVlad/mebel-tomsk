402ce2d1e91529451bab3a437650d73d9b086c96
OmetovVlad <ometov96@mail.ru>
2021-02-01 00:48:10 +0700

    Шапка баннер + меню

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+$('.header').on( 'click', '.open_menu', function() {
+	$(this).parents('.header').addClass('open');
+});
+
+$('.header').on( 'click', '.close_menu', function() {
+	$(this).parents('.header').removeClass('open');
+});
+
+$(document).mouseup(function (e){ // событие клика по веб-документу
+	var div = $(".header .menu nav"); // тут указываем ID элемента
+	if (!div.is(e.target) && div.has(e.target).length === 0) {
+		$('.header').removeClass('open');
+	}
+});
+
+// $('.banner-slider').slick({
+// 	slidesToShow: 1,
+// 	slidesToScroll: 1,
+// 	arrows: false,
+// 	fade: true,
+// 	asNavFor: '.text-slider'
+// });
+
+// $('.text-slider').slick({
+// 	dots: false,
+// 	arrows: false,
+// 	infinite: true,
+// 	asNavFor: '.banner-slider',
+// 	focusOnSelect: true
+// });
+
+$('.banner-slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: '.text-slider'
+});
+$('.text-slider').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	asNavFor: '.banner-slider',
+	arrows: false,
+	dots: false,
+	centerMode: false,
+	focusOnSelect: true
+});
